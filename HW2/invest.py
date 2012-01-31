6b7af9098c29f7c767df0b62572440d60362c5ca
Matt Dickenson <mcdickenson@gmail.com>
2012-01-31 08:26:27 -0500

    Added realStock class based on Corey Goldberg's ystockquote module.

--- HW2/invest.py
+++ HW2/invest.py
@@ -4,6 +4,7 @@ Spring 2012, Duke University
 Matt Dickenson """
 
 import random
+import ystockquote
 
 class Portfolio(object):
 
@@ -35,11 +36,11 @@ class Portfolio(object):
      
      def addCash(self, amount):
         self.cashBalance += amount
-        self.transactions += ("$%s added.\n" % amount)
+        self.transactions += ("$%.2f added.\n" % amount)
 
      def withdrawCash(self, amount):
         self.cashBalance -= amount
-        self.transactions += ("$%s withdrawn. \n" % amount)
+        self.transactions += ("$%.2f withdrawn. \n" % amount)
 
      def buyStock(self, shares, stock):
         purchase_amount = shares * stock.price
@@ -47,10 +48,10 @@ class Portfolio(object):
             self.cashBalance -= purchase_amount
             self.stockBalance[len(self.stockBalance)] = {"symbol": stock.name, "shares": shares, "object": stock}
             
-            self.transactions += ("Purchased %s shares of %s stock at $%s per share, for a total of $%s.\n" % (shares, stock.name, stock.price, purchase_amount))
+            self.transactions += ("Purchased %s shares of %s stock at $%.2f per share, for a total of $%.2f.\n" % (shares, stock.name, stock.price, purchase_amount))
 
         elif (shares % 1 == 0) & (purchase_amount > self.cashBalance):
-            return "That purchase exceeds your cash balance of %s. Please enter a smaller purchase." % self.cashBalance
+            return "That purchase exceeds your cash balance of %.2f. Please enter a smaller purchase." % self.cashBalance
 
         elif (shares % 1 != 0):
             return "Fractional shares cannot be purchased. Please enter a whole number of shares."
@@ -67,7 +68,7 @@ class Portfolio(object):
             stock_to_sell.calc_sell_price()
             total_sell_price = stock_to_sell.sell_price * share_amt
             self.cashBalance += total_sell_price
-            self.transactions += "Sold %s shares of %s at a price of $%s, for a total of $%s.\n" % (share_amt, symbol, stock_to_sell.sell_price, total_sell_price)
+            self.transactions += "Sold %s shares of %s at a price of $%.2f, for a total of $%.2f.\n" % (share_amt, symbol, stock_to_sell.sell_price, total_sell_price)
             self.stockBalance[x]['shares'] = 0
 
      def buyMutualFund(self, shares, fund):
@@ -76,7 +77,7 @@ class Portfolio(object):
             self.cashBalance -= purchase_amount
             self.mfBalance[len(self.mfBalance)] = {"symbol": fund.name, "shares": shares, "object": fund}
             
-            self.transactions += ("Purchased %s shares of %s fund at $%s per share, for a total of $%s.\n" % (shares, fund.name, fund.price, purchase_amount))
+            self.transactions += ("Purchased %s shares of %s fund at $%.2f per share, for a total of $%.2f.\n" % (shares, fund.name, fund.price, purchase_amount))
 
         elif (purchase_amount > self.cashBalance):
             return "That purchase exceeds your cash balance of %s. Please enter a smaller purchase." % self.cashBalance
@@ -93,7 +94,7 @@ class Portfolio(object):
             fund_to_sell.calc_sell_price()
             total_sell_price = fund_to_sell.sell_price * share_amt
             self.cashBalance += total_sell_price
-            self.transactions += "Sold %s shares of %s at a price of $%s, for a total of $%s.\n" % (share_amt, symbol, fund_to_sell.sell_price, total_sell_price)
+            self.transactions += "Sold %s shares of %s at a price of $%2f, for a total of $%.2f.\n" % (share_amt, symbol, fund_to_sell.sell_price, total_sell_price)
             self.mfBalance[x]['shares'] = 0
             
         else:
@@ -108,6 +109,16 @@ class Stock(object):
     def calc_sell_price(self):
         self.sell_price = self.price * random.uniform(0.5, 1.5)
         self.sell_price = round(self.sell_price, 2)
+
+class realStock(object):
+    def __init__(self, symbol):
+        self.price = float(ystockquote.get_price(symbol))
+        self.name = symbol
+        self.market_cap = ystockquote.get_market_cap(symbol)
+        self.ebitda = ystockquote.get_ebitda(symbol)
+        self.price_52_week_high = ystockquote.get_52_week_high(symbol)
+        self.price_52_week_low = ystockquote.get_52_week_low(symbol)
+        self.PE_ratio = ystockquote.get_price_earnings_ratio(symbol)
 
 class MutualFund(object):
 
