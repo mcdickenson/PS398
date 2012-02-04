b0ccc5f20d9e26a3a97d990b47bd363494683c1c
Matt Dickenson <mcdickenson@gmail.com>
2012-02-04 13:25:01 -0500

    Implemented listChecker and numChecker.

--- HW3/sorter.py
+++ HW3/sorter.py
@@ -6,7 +6,6 @@ Matt Dickenson """
 
 import random
 
-
 def selection(LIST): # complexity = O(N^2)
     while listChecker(LIST) & numChecker(LIST, 1): # makes sure we're dealing with list of numbers
         for index in range(len(LIST)):  # check over whole list
@@ -15,7 +14,6 @@ def selection(LIST): # complexity = O(N^2)
                 if LIST[sub_index] < LIST[minIndex]: # is there a smaller number?
                     minIndex = sub_index             # what is index of smaller number?
             swap(LIST, index, minIndex)
-            #LIST[index], LIST[minIndex] = LIST[minIndex], LIST[index] # swap
         return LIST
 
 def bubble(LIST): # complexity = O(n^2)
@@ -25,7 +23,6 @@ def bubble(LIST): # complexity = O(n^2)
             for sub_index in range(len(LIST)-1):            
                 if LIST[sub_index] > LIST[sub_index + 1]:
                     swap(LIST, sub_index, sub_index+1)
-                    #LIST[sub_index], LIST[sub_index + 1] = LIST[sub_index + 1], LIST[sub_index]
                     swaps =+ 1
             if swaps == 0: # optimization to quit once the list is in order
                 return LIST
@@ -33,12 +30,12 @@ def bubble(LIST): # complexity = O(n^2)
     return "Item is not list." # TODO: handle this as exception
         
 def quicksort(LIST): # complexity = O(NlogN) 
-    while listChecker(LIST) & numChecker(LIST, 1):
-        if LIST == []:
-            return []
+    while listChecker(LIST):
+        if LIST == []: 
+            return [] # stop recursion when an empty list is reached
         else:
             pivot_point = LIST[0]
-            lessThanPivot = quicksort([x for x in LIST[1:] if x < pivot_point])
+            lessThanPivot = quicksort([x for x in LIST[1:] if x < pivot_point]) # recursive
             greaterThanPivot = quicksort([x for x in LIST[1:] if x >= pivot_point])
             return lessThanPivot + [pivot_point] + greaterThanPivot
 
@@ -46,7 +43,7 @@ def swap(LIST, x, y):
     LIST[x], LIST[y] = LIST[y], LIST[x]
 
 def listChecker(item): # will check to verify item passed is list
-    return True
+    return isinstance(item, list)
 
 def numChecker(item, sub_item): # will check to verify item in list is number
-    return True
+    return isinstance(item[sub_item], (float, int))
