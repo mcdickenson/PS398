0ce3cb90cc951e94b65b078b47d076770b013dc9
Matt Dickenson <mcdickenson@gmail.com>
2012-02-04 19:33:12 -0500

    Implemented time checking for bubble sort and quick sort.

--- HW3/sorter.py
+++ HW3/sorter.py
@@ -7,37 +7,29 @@ Matt Dickenson """
 import random
 
 def selection(LIST): # complexity = O(N^2)
-    while listChecker(LIST) & numChecker(LIST, 0): # makes sure we're dealing with list of numbers
-        for index in range(len(LIST)):  # check over whole list
-            minIndex = index            # end of ordered portion
-            for sub_index in range(index+1, len(LIST)): # check between ordered portion thru end of list
-                if LIST[sub_index] < LIST[minIndex]: # is there a smaller number?
-                    minIndex = sub_index             # what is index of smaller number?
-            swap(LIST, index, minIndex)
-        return LIST
+    for index in range(len(LIST)):  # check over whole list
+        minIndex = index            # end of ordered portion
+        for sub_index in range(index+1, len(LIST)): # check between ordered portion thru end of list
+            if LIST[sub_index] < LIST[minIndex]: # is there a smaller number?
+                minIndex = sub_index             # what is index of smaller number?
+        swap(LIST, index, minIndex)
+    return LIST
 
 def bubble(LIST): # complexity = O(n^2)
-    while listChecker(LIST) & numChecker(LIST, 1):
-        for index in range(len(LIST)):
-            swaps = 0
-            for sub_index in range(len(LIST)-1):            
-                if LIST[sub_index] > LIST[sub_index + 1]:
-                    swap(LIST, sub_index, sub_index+1)
-                    swaps =+ 1
-            if swaps == 0: # optimization to quit once the list is in order
-                return LIST
-        return LIST
-    return "Item is not list." # TODO: handle this as exception
+    for index in range(len(LIST)):
+        for sub_index in range(len(LIST)-1):            
+            if LIST[sub_index] > LIST[sub_index + 1]:
+                swap(LIST, sub_index, sub_index+1)
+    return LIST
         
 def quicksort(LIST): # complexity = O(NlogN) 
-    while listChecker(LIST):
-        if LIST == []: 
-            return [] # stop recursion when an empty list is reached
-        else:
-            pivot_point = LIST[0]
-            lessThanPivot = quicksort([x for x in LIST[1:] if x < pivot_point]) # recursive
-            greaterThanPivot = quicksort([x for x in LIST[1:] if x >= pivot_point])
-            return lessThanPivot + [pivot_point] + greaterThanPivot
+    if LIST == []: 
+        return [] # stop recursion when an empty list is reached
+    else:
+        pivot_point = LIST[0]
+        lessThanPivot = quicksort([x for x in LIST[1:] if x < pivot_point]) # recursive
+        greaterThanPivot = quicksort([x for x in LIST[1:] if x >= pivot_point])
+        return lessThanPivot + [pivot_point] + greaterThanPivot
 
 def swap(LIST, x, y):
     LIST[x], LIST[y] = LIST[y], LIST[x]
--- HW3/sorter_times.py
+++ HW3/sorter_times.py
@@ -4,26 +4,50 @@ import math
 import time
 import sorter
 
+
+# recorded time will be average of 100 trials on 90 random lists
+maxDraws = 100
+repetitions = 100
+
 selection_sort_times = []
 bubble_sort_times = []
 quick_sort_times = []
-sort_times_N = range(10,100)
+sort_N = range(10, maxDraws)
 
 
-for draw in range(10,100):
-    toSort = random.sample(range(0,100), int(draw))
+# Selection sort
+for draw in range(10,maxDraws):
+    toSort = random.sample(range(0,maxDraws), int(draw))
     start= time.clock()
-    for attempt in range(100):
+    for attempt in range(repetitions):
         sorter.selection(toSort)
     end= time.clock()
-    selection_sort_times.append((end-start)/100)
-    # recorded time will be average of 100 trials on 90 random lists
-  
+    selection_sort_times.append((end-start)/repetitions)
+
+# Bubble Sort
+for draw in range(10,maxDraws):
+    toSort = random.sample(range(0,maxDraws), int(draw))
+    start= time.clock()
+    for attempt in range(repetitions):
+        sorter.bubble(toSort)
+    end= time.clock()
+    bubble_sort_times.append((end-start)/100)
+
+# Quick Sort (well, we'll see...)
+for draw in range(10,maxDraws):
+    toSort = random.sample(range(0,maxDraws), int(draw))
+    start= time.clock()
+    for attempt in range(repetitions):
+        sorter.quicksort(toSort)
+    end= time.clock()
+    quick_sort_times.append((end-start)/100)
+      
 #Write these times to a csv names times.csv where each time gets its own column
 csvWriter = csv.writer(open('times.csv', 'wb'))
 
+print len(selection_sort_times), len(bubble_sort_times), len(quick_sort_times)
 for i in range(0,len(selection_sort_times)):
-  csvWriter.writerow([selection_sort_times[i]])
+  csvWriter.writerow([sort_N[i], selection_sort_times[i], bubble_sort_times[i], quick_sort_times[i]])
 
 
 
