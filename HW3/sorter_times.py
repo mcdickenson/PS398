7c58a4a9a52d3b2b42550188796096ec235346f2
Matt Dickenson <mcdickenson@gmail.com>
2012-02-04 19:46:10 -0500

    Moved random draw of lists into the iteration for more accurate average time.

--- HW3/sorter_times.py
+++ HW3/sorter_times.py
@@ -17,27 +17,27 @@ sort_N = range(10, maxDraws)
 
 # Selection sort
 for draw in range(10,maxDraws):
-    toSort = random.sample(range(0,maxDraws), int(draw))
     start= time.clock()
     for attempt in range(repetitions):
+        toSort = random.sample(range(0,maxDraws), int(draw))
         sorter.selection(toSort)
     end= time.clock()
     selection_sort_times.append((end-start)/repetitions)
 
 # Bubble Sort
 for draw in range(10,maxDraws):
-    toSort = random.sample(range(0,maxDraws), int(draw))
     start= time.clock()
     for attempt in range(repetitions):
+        toSort = random.sample(range(0,maxDraws), int(draw))
         sorter.bubble(toSort)
     end= time.clock()
     bubble_sort_times.append((end-start)/100)
 
 # Quick Sort (well, we'll see...)
 for draw in range(10,maxDraws):
-    toSort = random.sample(range(0,maxDraws), int(draw))
     start= time.clock()
     for attempt in range(repetitions):
+        toSort = random.sample(range(0,maxDraws), int(draw))
         sorter.quicksort(toSort)
     end= time.clock()
     quick_sort_times.append((end-start)/100)
@@ -45,9 +45,6 @@ for draw in range(10,maxDraws):
 #Write these times to a csv names times.csv where each time gets its own column
 csvWriter = csv.writer(open('times.csv', 'wb'))
 
-print len(selection_sort_times), len(bubble_sort_times), len(quick_sort_times)
 for i in range(0,len(selection_sort_times)):
   csvWriter.writerow([sort_N[i], selection_sort_times[i], bubble_sort_times[i], quick_sort_times[i]])
-
-
 
