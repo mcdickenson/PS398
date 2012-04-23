380c72bac262246bf605c7ceec13cb6bbf6a1712
Matt Dickenson <mcdickenson@gmail.com>
2012-04-23 11:13:10 -0400

    Added drawArrow to slopefieldPlot.

--- FinalProj/slopefieldPlot.py
+++ FinalProj/slopefieldPlot.py
@@ -35,10 +35,6 @@ def slopefieldPlotter(xMin, xMax, xInterval, yMin, yMax, yInterval, dXy, dYx, xL
     # save file
     pngSave(filename)
 
-def pngSave(name):
-    pngName = str(name) + '.png'
-    pyplot.savefig(pngName)
-
 # draw curve (line) taking initial value
 def drawVector(dXx, dXy, dYx, dYy, X0, Y0, numsteps, stepsize=0.01, colorVar='b'): # currently implemented for implicit diffEq's
     xCoords = [X0]
@@ -64,6 +60,43 @@ def drawVector(dXx, dXy, dYx, dYy, X0, Y0, numsteps, stepsize=0.01, colorVar='b'
 #TODO: make it easier to pass coefficient parameters
 # will need dX, dY w/r/t X,Y,dX,dY,t
 
+def getEndVector(dXx, dXy, dYx, dYy, X0, Y0, numsteps, stepsize=0.01): # find where vector ends
+    # compute coordinates using Euler's method
+    for i in range(1,numsteps): # maybe numsteps+1
+        if i == 1: # first time thru
+            x_tk = X0+((dXx*X0)+(dXy*X0))*stepsize
+            y_tk = Y0+((dYx*X0)+(dYy*Y0))*stepsize
+        else:
+            x_tk_last = x_tk
+            y_tk_last = y_tk
+            x_tk = x_tk_last+((dXx*x_tk_last)+(dXy*y_tk_last))*stepsize
+            y_tk = y_tk_last+((dYx*x_tk_last)+(dYy*y_tk_last))*stepsize
+
+    return round(x_tk, 2), round(y_tk,2) # may want to avoid rounding
+
+def drawArrow(x1, x2, y1, y2, headlength=0.05, direction='R', lineColor='r'):
+    pyplot.plot([x1, x2], [y1, y2], color = lineColor)
+
+    if direction == 'R':
+        pyplot.plot([x2,x2-(headlength*2)],[y2,y2+headlength], color=lineColor)
+        pyplot.plot([x2,x2-(headlength*2)],[y2,y2-headlength], color=lineColor)
+
+    elif direction == 'L':
+        pyplot.plot([x2,x2+(headlength*2)],[y2,y2+headlength], color=lineColor)
+        pyplot.plot([x2,x2+(headlength*2)],[y2,y2-headlength], color=lineColor)
+
+    elif direction == 'U':
+        pyplot.plot([x2,x2-headlength], [y2,y2-(headlength*2)], color=lineColor)
+        pyplot.plot([x2,x2+headlength], [y2,y2-(headlength*2)], color=lineColor)
+
+    elif direction == 'D':
+        pyplot.plot([x2,x2-headlength], [y2,y2+(headlength*2)], color=lineColor)
+        pyplot.plot([x2,x2+headlength], [y2,y2+(headlength*2)], color=lineColor)
+
+def pngSave(name):
+    pngName = str(name) + '.png'
+    pyplot.savefig(pngName)
+
 # Examples
 # Iwo Jima, no reinforcements
 slopefieldPlotter(0, 6, .2, 0, 3, .2, -0.05, -0.01, xLab='US', yLab='Japan', mainLab='Lanchester Squares, No Reinforcements', drawAxes=True, filename='lanchTester')
@@ -74,12 +107,17 @@ pngSave('lanchTester2')
 slopefieldPlotter(0, 6, .2, 0, 3, .2, -0.05, -0.01, xLab='US', yLab='Japan', mainLab='Lanchester Squares, With Reinforcements', drawAxes=True, filename='reinfTester')
 drawVector(0, -0.05, -0.01, 0, 5.4, 2.15, 300, stepsize=0.1, colorVar='b')
 pngSave('reinfTester2')
-pyplot.plot([3.2,5.05],[.9,.9], color='r')
-pyplot.plot([5.05,4.9],[.9,.95], color='r')
-pyplot.plot([5.05,4.9],[.9,.85], color='r')
+drawArrow(3.2, 5.05, .9, .9, headlength=0.05, direction='R', lineColor='r')
+#pyplot.plot([3.2,5.05],[.9,.9], color='r')
+#pyplot.plot([5.05,4.9],[.9,.95], color='r')
+#pyplot.plot([5.05,4.9],[.9,.85], color='r')
 pngSave('reinfTester3')
 drawVector(0, -0.05, -0.01, 0, 5.1, 0.9, 200, stepsize=0.1, colorVar='b')
 pngSave('reinfTester4')
+drawArrow(1, 0, .9, .9, headlength=0.05, direction='L', lineColor='g')
+drawArrow(1, 1, .5, 2.0, headlength=0.1, direction='U', lineColor='b')
+drawArrow(2, 2, 2.5, 1.0, headlength=0.01, direction='D', lineColor='y')
+pngSave('arrowTester')
 
 # TODO: for reinforcements, have an option to return last xcoord, ycoord
 
